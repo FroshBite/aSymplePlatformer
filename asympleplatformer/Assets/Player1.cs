000bc7897b439c6b8e63697271f08dd8a2742f49
Serge Babayan <serj.b.babayan@gmail.com>
2014-10-04 15:56:27 -0400

    implemented boost

--- asympleplatformer/Assets/Player1.cs
+++ asympleplatformer/Assets/Player1.cs
@@ -2,26 +2,36 @@
 using System.Collections;
 
 public class Player1 : MonoBehaviour {
-	
-	public float speed=1, gravity=1, jumpScale=1;
+
+	void ResetPosition(){
+		this.transform.position = startPoint;
+		this.rigidbody2D.velocity = Vector2.zero;
+	}
+
+	public float gravity=1, jumpScale=1, speed=1,turboFactor=1f;	
 	new Vector2 startPoint;
+
+
+	
 	// Use this for initialization
 	void Start () {
+
 		startPoint = transform.position;
 
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float right=0, left=0, up=0, down=0;
-		this.rigidbody2D.gravityScale = 1.0f;
+		float right = 0f, left = 0f, up = 0f, turbo = 1.0f, turboY = 1.0f;
 
 		if (Input.GetKey(KeyCode.D)){
 			right=1.0f;
 		}
+
 		if (Input.GetKey(KeyCode.A)){
 			left=-1.0f;
 		}
+
 		if ((Input.GetKey (KeyCode.Space) && rigidbody2D.velocity.y == 0)){
 			up=6.0f;
 		}
@@ -30,13 +40,19 @@ public class Player1 : MonoBehaviour {
 			up=-2.0f;
 		}
 
+		if (Input.GetKey (KeyCode.LeftShift)){
+			turbo=turboFactor;
+			turboY=0f;
+
+			
+		}
+
 		if (Input.GetKeyDown (KeyCode.R)){
-			this.transform.position=startPoint;
-			this.rigidbody2D.velocity=Vector2.zero;
+			ResetPosition();
 		}
 
 
-		rigidbody2D.velocity = new Vector2 (right * speed + left * speed, rigidbody2D.velocity.y - gravity  + up * jumpScale);
+		rigidbody2D.velocity = new Vector2 (right*speed*turbo + left * speed*turbo, turboY* (rigidbody2D.velocity.y - gravity  + up * jumpScale));
 	
 	}
 
