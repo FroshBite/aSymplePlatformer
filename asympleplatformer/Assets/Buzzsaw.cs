4b5854534dba4e47802f96c7b1e4c16e4c1c2a1b
Kevin Xing <kxing@uwaterloo.ca>
2014-10-04 19:02:01 -0400

    buzzsaws are now coins (will fix)

---
+++ asympleplatformer/Assets/Buzzsaw.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buzzsaw : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.name == "Player1") {
+			GameObject.Destroy(coll.gameObject);
+		}
+	}
+	
+}
