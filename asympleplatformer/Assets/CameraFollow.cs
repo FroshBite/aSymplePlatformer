557f0123ea19a9ed6d5211b2d91944e383cd3066
Serge Babayan <serj.b.babayan@gmail.com>
2014-10-04 19:41:15 -0400

    no changes

---
+++ asympleplatformer/Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform player;
+	public float number =1.0f;
+	private Vector2 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += player.position + offset;
+	}
+}
