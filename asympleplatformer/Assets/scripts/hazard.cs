9771d52a3ee5156fc9d0981c4440694d0d10a82e
Serge Babayan <serj.b.babayan@gmail.com>
2014-10-05 13:11:03 -0400

    fixed skeleton

--- asympleplatformer/Assets/scripts/hazard.cs
+++ asympleplatformer/Assets/scripts/hazard.cs
@@ -2,40 +2,28 @@
 using System.Collections;
 
 public class hazard : MonoBehaviour {
-	bool isGrabbed = false;
-	bool isFlying = false;
-	bool walk;
+	bool isGrabbed = false, isFlying = false, walk =false;
 	float walkVel;
 	Vector2 startPoint;
-	public int speed;
-	public int gravity;
+	public float speed=1.0f, gravity=1.0f;
 	public Transform player1;
 
 	void Start () {
 		walk = false;
 		startPoint = transform.position;
 		ResetPosition();
+		rigidbody2D.gravityScale = 0;
 
 	}
 
 
 	void ResetPosition(){
-		this.transform.position = startPoint;
 		rigidbody2D.velocity = Vector2.zero;
+		rigidbody2D.gravityScale = 0;
 		walk = false;
-		//this.rigidbody2D.gravityScale = 0;
 		isGrabbed = false;
 		isFlying = false;
-	}
-	
-
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.R)){
-			this.isGrabbed = false;
-			this.isFlying = false;
-			this.rigidbody2D.velocity = Vector2.zero;
-			ResetPosition();
-		}
+		this.transform.position = startPoint;
 	}
 
 	void FixedUpdate(){
@@ -43,41 +31,36 @@ public class hazard : MonoBehaviour {
 		
 			if(walkVel>0){
 				transform.localScale = new Vector3(-30,30,30);
-			}else{
-				transform.localScale = new Vector3(30,30,30);
+				rigidbody2D.velocity = new Vector2 (speed,rigidbody2D.velocity.y-gravity);
 			}
-		
-		
+			else{
+				transform.localScale = new Vector3(30,30,30);
+				rigidbody2D.velocity = new Vector2 (-speed,rigidbody2D.velocity.y-gravity);
+			}
 		}
 
-		if(isGrabbed){
+		if(isGrabbed){ //moves the skeleton along with the mouse
 			Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			worldPosition.z = 0;
-			//Debug.Log ("World Position: " + worldPosition.ToString());
 			this.transform.position = worldPosition;
 		}
 		if(isFlying)
-			rigidbody2D.velocity = new Vector2 (speed*walkVel,-gravity);
+			rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x,rigidbody2D.velocity.y-gravity);
 
-		
-		//if(!isFlying){
-		//	this.rigidbody2D.gravityScale = 0;
-		//} else {
-		//	this.rigidbody2D.gravityScale = gravity;
-
-		//}
-
-
+		if (rigidbody2D.position.y < -50) {
+			ResetPosition ();
+		}
 	}
 	
 	void OnMouseDown(){
 		isGrabbed = true;
 		isFlying = false;
-		this.collider2D.enabled = false;
 		walk = false;
+		this.collider2D.enabled = false;
 	}
 	
 	void OnMouseUp(){
+
 		if(isGrabbed){
 			if(Vector2.Distance(player1.position, this.transform.position)<5){
 				this.transform.position = player1.position + new Vector3(5, 0, 0);
@@ -85,11 +68,8 @@ public class hazard : MonoBehaviour {
 
 			isGrabbed = false;
 			isFlying = true;
-			rigidbody2D.velocity = new Vector2(0,0);
 			this.collider2D.enabled = true;
 			walkVel = (player1.position.x)-this.transform.position.x;
-			walkVel /= Mathf.Abs(walkVel);
-			
 		}
 	}
 	
